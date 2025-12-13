50e85a7da819f22c52e8e06df75f7f3f8f7b43f1
Liz Tan <elizabethsztan@gmail.com>
2025-12-13 18:03:11 +0000

    set cache to be in stores on HPC

--- load_qwen.py
+++ load_qwen.py
@@ -2,8 +2,17 @@
 Script to load and interact with Qwen2.5-1.5B-Instruct model
 """
 
+import os
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Set HuggingFace cache directory to cephfs when on HPC to avoid quota issues
+# Check if we're on the HPC by looking for the cephfs directory
+if os.path.exists('/cephfs/store/gr-mc2473/eszt2'):
+    os.environ['HF_HOME'] = '/cephfs/store/gr-mc2473/eszt2/hf_cache'
+    os.environ['TRANSFORMERS_CACHE'] = '/cephfs/store/gr-mc2473/eszt2/hf_cache'
+    os.environ['HF_DATASETS_CACHE'] = '/cephfs/store/gr-mc2473/eszt2/hf_cache/datasets'
+    print("Running on HPC - using cephfs cache directory")
 
 def load_qwen_model(device="mps"):
     """
